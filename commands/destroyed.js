73ea7a82c750f3d6f194c92565304b7d5009381b
Markay12 <ashinhust.brass@gmail.com>
2020-10-16 11:27:39 -0700

    command to add destroyed gif's

---
+++ commands/destroyed.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const colors = require("../colors.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let gifs = [
+
+        "https://media.giphy.com/media/26FfaYF8aDEmaJMdy/giphy.gif",
+        "https://media.giphy.com/media/26tjZQs8GhNsXA7p6/giphy.gif",
+        "https://media.giphy.com/media/5qoRdabXeT4GY/giphy.gif"
+
+    ]
+
+    let pick = gifs[Math.floor(Math.random() * gifs.length)];
+
+    let embed = new Discord.MessageEmbed();
+    embed.setColor(colors.orange);
+    embed.setImage(pick);
+
+    if(args[0]){
+
+        embed.setTitle(args[0] + " got DESTROYED!");
+
+
+    } else {
+
+        embed.setTitle(`DESTRUCTION!!`);
+
+    }
+
+    message.channel.send(embed);
+
+}
+
+module.exports.help = {
+
+    name: "destruction",
+    aliases: []
+
+}
