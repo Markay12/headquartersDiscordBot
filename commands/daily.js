d95a7d2f50848bfdce224a76ed00c5713150d0c3
Markay12 <ashinhust.brass@gmail.com>
2020-10-20 16:29:02 -0700

    remove unnecessary vars

--- commands/daily.js
+++ commands/daily.js
@@ -1,8 +1,4 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const money = require("../money.json");
 const ms = require("parse-ms");
-const cooldowns = require("../cooldowns.json");
 const mongoose = require("mongoose");
 const botconfig = require("../botconfig.json");
 
