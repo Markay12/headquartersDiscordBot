a78de73298d746f0d9e33afb94e8ab953875a47a
Markay12 <ashinhust.brass@gmail.com>
2020-10-20 16:58:52 -0700

    updated for server access

--- commands/adminpay.js
+++ commands/adminpay.js
@@ -1,57 +1,67 @@
-const money = require("../money.json");
-const fs = require("fs");
-const { parse } = require("path");
+const maxBet = 100000;
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//CONNECT TO MONGOOSEDB
+mongoose.connect(botconfig.passmongodb, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
+//MODELS
+const Data = require("../models/data.js")
 
 module.exports.run = async (bot, message, args) => {
-
-    if(message.author.id != "580894635606409228") return message.reply("You cannot use this command")
 
     let user = message.mentions.members.first() || bot.users.cache.get(args[0]); //get user by mention
     if(!user) return message.reply("Sorry, couldn't find that member. Make sure they haven't been laid off... \n*we've had some recent budget cuts*");
 
-    if(!args[1]) return message.reply("Please specify the amount you would like to donate");
-    
 
-    if(!money[user.id])
-    {
+    Data.findOne({
 
-        money[user.id] = 
+        userID: user.id
+
+    }, (err, userData) => {
+
+        if(err) console.log(err);
+
+        if(!args[1]) return message.reply("Please specify the amount you would like to donate");
+
+        if(!userData)
         {
 
-            name: bot.users.cache.get(user.id).tag,
-            money: parseInt(args[1])
+            const newData = new Data({
+
+                name: bot.users.cache.get(user.id).username,
+                userID: user.id,
+                leaderboard: "all",
+                money: parseInt(args[1]),
+                daily: 0,
+
+
+            })
+            newData.save().catch(err => console.log(err));
+
+
+        } else {
+
+            userData.money += parseInt(args[1]);
+            userData.save().cathc(err => console.log(err));
+
 
         }
 
+        return message.channel.send(`${message.author.username} balance is now ${user.money} : ${args[1]} ♏︎ `);
 
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-
-            if(err) console.log(err);
-
-        });
-
-    } else
-    {
-
-        money[user.id].money += parseInt(args[1]);
-
-
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-
-            if(err) console.log(err);
-
-        });
-
-    }
-
-    return message.channel.send(`${message.author.username} admin payed ♏︎ ${args[1]} to ${bot.users.cache.get(user.id).username}`);
-
-    
+    })
+  
 }
 
 module.exports.help = {
 
     name: "adminpay",
-    aliases: []
+    aliases: ["ap"]
 
 }
