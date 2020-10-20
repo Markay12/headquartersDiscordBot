746069447d1ab78f343d09e53a6f3fb7e6b937b6
Markay12 <ashinhust.brass@gmail.com>
2020-10-20 14:47:39 -0700

    updated with mongodb constants

--- commands/balance.js
+++ commands/balance.js
@@ -1,6 +1,18 @@
 const { Client } = require("discord.js");
 const fs = require("fs");
 const money = require("../money.json");
+const mongoose = require("mongoose"); //add new package for database
+const botconfig = require("../botconfig.json");
+
+//CONNECT TO MONGOOSEDB
+mongoose.connect(botconfig.passmongodb, {
+
+    useNewUrlParser: true
+
+});
+
+//MODELS
+const Data = require("../models/data.js")
 
 module.exports.run = async (bot, message, args) => {
 
