e00e1f4e668897ca6db140218af87c0149415318
Markay <54555076+Markay12@users.noreply.github.com>
2020-10-22 08:09:11 -0700

    updated with new commands

--- commands/help.js
+++ commands/help.js
@@ -12,13 +12,18 @@ module.exports.run = async (bot, message, args) => {
                          .ping ---> You can use this to make sure I'm awake and check latency between us\n
                          .help ---> Well... you got here somehow???\n
                          .celebrate <name> ---> let's celebrate! Woohoo (Shows a gif celebration)\n
-                         .destruction <name> --> destruction gif\n\n
+                         .destruction <name> --> destruction gif\n
+                         .clear –> allows the user to clear a certain amount of messages (max 99 messages)\n
                          Economy\n---------------------
                          **YOU HAVE TO USE COMMAND \`.bal\` before you can claim your daily rewards**
                          .bal ---> Check your server balance for daily rewards and betting!\n
                          .daily ---> Collect your daily rewards balance here!\n
                          .donate <@user> ---> Donate money to a certain user\n
-                         .gamble <amount> or *.gamble all* ---> win or lose it all! See where you can take your money!\n\nFeel free to send CEO Markay#5500 or COO FeatheredSnake#6504 any questions or things you would like implemented in the future!`);
+                         .gamble <amount> or *.gamble all* ---> win or lose it all! See where you can take your money!\n\n
+                         Rank System\n---------------------\n
+                         .ranks ---> Displays which ranks the user can buy and upgrade\n
+                         .purchase ---> purchase roles within the server\n\n
+                         Feel free to send CEO Markay#5500 or COO FeatheredSnake#6504 any questions or things you would like implemented in the future!`);
 
 
 
