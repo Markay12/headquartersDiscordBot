fa8ed9aff12c22386af849aeefeef41e7038a8e0
Markay <54555076+Markay12@users.noreply.github.com>
2020-10-22 07:57:08 -0700

    updated pricing

--- commands/ranks.js
+++ commands/ranks.js
@@ -7,9 +7,9 @@ module.exports.run = async (bot, message, args) => {
 
     embed.setDescription(`RISE THROUGH THE RANKS!\n
                         Use .purchase <rankname> to purchase each rank and move your way through the server!\n-------------------------------------------\n
-                        Wandering --------- 250 ♏︎\n
-                        Temp --------- 1000 ♏︎\n
-                        Intern --------- 2000 ♏︎\n`);
+                        Wandering --------- 100 ♏︎\n
+                        Temp --------- 500 ♏︎\n
+                        Intern --------- 1000 ♏︎\n`);
 
 
     embed.setColor(color.gold);
