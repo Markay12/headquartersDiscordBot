65c7a06ccd071eb905a5d5e11da7dbf144d01fc8
Markay12 <ashinhust.brass@gmail.com>
2020-10-21 17:03:25 -0700

    updated to purchase two ranks

--- commands/purchase.js
+++ commands/purchase.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const money = require("../money.json")
 const mongoose = require("mongoose")
 const botconfig = require("../botconfig.json")
+const color = require("../colors.json")
+const Discord = require("discord.js");
 
 //CONNECT TO MONGOOSEDB
 mongoose.connect(botconfig.passmongodb, {
@@ -19,8 +21,10 @@ module.exports.run = async (bot, message, args) => {
 
     if(!args[0]) return message.reply("Please specify which role you would like to purchase")
 
-    let pillageRole = '768585859192586251';
-    var pillagerPrice = 250; //set price
+    let wanderRole = '768618964774682645';
+    var wanderPrice = 250; //set price
+    let tempRole = '768619085151469608';
+    let tempPrice = 1000;
 
     let user = message.author; //user can only apply role to themselves
 
@@ -50,22 +54,28 @@ module.exports.run = async (bot, message, args) => {
         else 
         {
 
-            if (args[0].toLowerCase() == "pillager") 
+            if (args[0].toLowerCase() == "wander" || args[0].toLowerCase() == "wandering") 
             {
                 //what if the user can try and purchase this role
                 
 
                 //lets buy pillager rank for 150
-                if (data.money >= pillagerPrice)
+                if (data.money >= wanderPrice)
                 {
 
                     //if they already have the role
-                    if(message.member.roles.cache.has(pillageRole)) return message.reply("You already have this role");
+                    if(message.member.roles.cache.has(wanderRole)) return message.reply("You already have this role");
 
-                    message.member.roles.add(pillageRole).catch(console.error);
-                    data.money -= pillagerPrice;
+                    message.member.roles.add(wanderRole).catch(console.error);
+                    data.money -= wanderPrice;
                     data.save().catch(err => console.log(err));
-                    return message.reply(`*You have just purchased the Pillager Role for 150 ♏︎*\n Welcome to the land of the pillagers. Long way to move up but a great start indeed!`);
+
+                    let wanderEmbed = new Discord.MessageEmbed();
+
+                    wanderEmbed.setDescription(`*You have just purchased the Wandering Role for 150 ♏︎*\n Welcome to the server! You've now found a place and ready to move on up!\n Speak with members, get yourself known`)
+                    wanderEmbed.setColor(color.brown);
+
+                    return message.reply(wanderEmbed);
 
                 } else {
 
@@ -73,7 +83,36 @@ module.exports.run = async (bot, message, args) => {
 
                 }
 
+            } else if (args[0].toLowerCase() == "temp")
+            {
+
+                //if they are already a wanderer or if they don't have pillager yet
+                if(message.member.roles.cache.has(tempRole)) return message.reply("You already have this role");
+                if(!message.member.roles.cache.has(wanderRole)) return message.reply("You can't buy this role until you purchase Wandering");
+
+                if(data.money >= 1000)
+                {
+
+                    message.member.roles.add(tempRole).catch(console.error);
+                    message.member.roles.remove(wanderRole).catch(console.error);
+                    data.money -= tempPrice;
+                    data.save().catch(err => console.log(err));
+
+                    let tempEmbed = new Discord.MessageEmbed();
+
+                    tempEmbed.setDescription(`*You have just purchased the Temp Role for 1000 ♏︎*\n Welcome! Make sure to change the printer ink. \nI like 2 packets of sugar in my coffee and a splash of cream. Not too cold and not too warm.. just about 195 degrees F\n`);
+
+                    tempEmbed.setColor(color.cream);
+                    return message.reply(tempEmbed);
+
+                } else {
+
+                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs 1000 ♏︎`)
+
+                }
+
             }
+
         }
 
     })
