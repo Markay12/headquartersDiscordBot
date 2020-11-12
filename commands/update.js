714fbb219fc0dc26b0082270e8df9f7fa3be6153
Markay12 <ashinhust.brass@gmail.com>
2020-11-12 14:58:54 -0700

    updated for new update

--- commands/update.js
+++ commands/update.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
     let embed = new Discord.MessageEmbed();
 
-    embed.setDescription("New Bot Updates as of 05 November 2020\n\n**Added New Roles!**\nDesk Agent, Junior Developer and Senior Developer are available for Purchase\n\nDaily functionality is set for each role and will update for each new role.\nDesk Agent Recieves 1500\nJunior Developer Recieves 2000 and Senior Developer recieves 3000");
+    embed.setDescription("New Bot Updates as of 12 November 2020\n\n**Added New Role Information!**\n.json file includes all role information. New role available for purchase Senior Developer which costs 20,000.\n Modules and .ranks has been updated as well\n\nContact Markay#5500 for any more information");
 
     embed.setColor(color.red);
     return message.channel.send(embed);
