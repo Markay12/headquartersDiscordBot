64b74ec954de6f4a7911aa198347c29001334d11
Markay12 <ashinhust.brass@gmail.com>
2020-11-12 14:29:53 -0700

    updated with new json file information

--- commands/ranks.js
+++ commands/ranks.js
@@ -1,5 +1,6 @@
 const color = require("../colors.json");
 const Discord = require("discord.js");
+const role = require("../roles.json");
 
 module.exports.run = async (bot, message, args) => {
 
@@ -7,9 +8,13 @@ module.exports.run = async (bot, message, args) => {
 
     embed.setDescription(`RISE THROUGH THE RANKS!\n
                         Use .purchase <rankname> to purchase each rank and move your way through the server!\n-------------------------------------------\n
-                        Wandering --------- 100 ♏︎\n
-                        Temp --------- 500 ♏︎\n
-                        Intern --------- 1000 ♏︎\n`);
+                        Applicant ------------- FREE\n
+                        Wandering ------------- ${role.wander.price} ♏︎\n
+                        Temp ------------------ ${role.temp.price} ♏︎\n
+                        Intern --------------- ${role.intern.price} ♏︎\n
+                        Desk Agent ----------- ${role.deskAgent.price} ♏︎\n
+                        Junior Developer ---- ${role.juniorDev.price} ♏︎\n
+                        Senior Developer ---- ${role.seniorDev.price} ♏︎\n`);
 
 
     embed.setColor(color.gold);
