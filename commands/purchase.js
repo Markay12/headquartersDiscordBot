f6653a4189da283faaa310370136e984e3d6601a
Markay12 <ashinhust.brass@gmail.com>
2020-11-12 14:20:00 -0700

    updated with new roles.json file components for greater functionality

--- commands/purchase.js
+++ commands/purchase.js
@@ -1,8 +1,9 @@
 const { Client } = require("discord.js");
-const mongoose = require("mongoose")
-const botconfig = require("../botconfig.json")
-const color = require("../colors.json")
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+const color = require("../colors.json");
 const Discord = require("discord.js");
+const role = require("../roles.json");
 
 //CONNECT TO MONGOOSEDB
 mongoose.connect(botconfig.passmongodb, {
@@ -16,21 +17,6 @@ mongoose.connect(botconfig.passmongodb, {
 const Data = require("../models/data.js")
 
 module.exports.run = async (bot, message, args) => {
-
-
-    let wanderPrice = 100; //set price
-    let tempPrice = 500;
-    let internPrice = 1000;
-    let deskAgentPrice = 5000;
-    let juniorPrice = 10000;
-    let seniorPrice = 20000; 
-    let applicantRole = '768858705197531156';
-    let wanderRole = '768618964774682645';
-    let tempRole = '768619085151469608';
-    let internRole = '768650304941719582';
-    let deskAgentRole = '774106270130241566';
-    let juniorRole = '763565026006269982';
-    let seniorRole = '763564784104112130';
 
     let user = message.author; //user can only apply role to themselves
 
@@ -60,19 +46,19 @@ module.exports.run = async (bot, message, args) => {
         else 
         {
 
-            if (message.member.roles.cache.has(applicantRole)) 
+            if (message.member.roles.cache.has(role.applicant.role)) 
             {
                 //what if the user can try and purchase this role
                 
 
                 //lets buy pillager rank for 150
-                if (data.money >= wanderPrice)
+                if (data.money >= role.wander.price)
                 {
 
 
-                    message.member.roles.add(wanderRole).catch(console.error);
-                    message.member.roles.remove(applicantRole).catch(console.error);
-                    data.money -= wanderPrice;
+                    message.member.roles.add(role.wander.role).catch(console.error);
+                    message.member.roles.remove(role.applicant.role).catch(console.error);
+                    data.money -= role.wander.price;
                     data.save().catch(err => console.log(err));
 
                     let wanderEmbed = new Discord.MessageEmbed();
@@ -88,43 +74,43 @@ module.exports.run = async (bot, message, args) => {
 
                 }
 
-            } else if (message.member.roles.cache.has(wanderRole))
+            } else if (message.member.roles.cache.has(role.wander.role))
             {
 
-                if(!message.member.roles.cache.has(wanderRole)) return message.reply("You can't buy this role until you purchase Wandering");
+                if(!message.member.roles.cache.has(role.wander.role)) return message.reply("You can't buy this role until you purchase Wandering");
 
-                if(data.money >= tempPrice)
+                if(data.money >= role.temp.price)
                 {
 
-                    message.member.roles.add(tempRole).catch(console.error);
-                    message.member.roles.remove(wanderRole).catch(console.error);
-                    data.money -= tempPrice;
+                    message.member.roles.add(role.temp.role).catch(console.error);
+                    message.member.roles.remove(role.wander.role).catch(console.error);
+                    data.money -= role.temp.price;
                     data.save().catch(err => console.log(err));
 
                     let tempEmbed = new Discord.MessageEmbed();
 
-                    tempEmbed.setDescription(`*You have just purchased the Temp Role for ${tempPrice} ♏︎*\n Welcome! Make sure to change the printer ink. \nI like 2 packets of sugar in my coffee and a splash of cream. Not too cold and not too warm.. just about 195 degrees F\n`);
+                    tempEmbed.setDescription(`*You have just purchased the Temp Role for ${role.temp.price} ♏︎*\n Welcome! Make sure to change the printer ink. \nI like 2 packets of sugar in my coffee and a splash of cream. Not too cold and not too warm.. just about 195 degrees F\n`);
 
                     tempEmbed.setColor(color.cream);
                     return message.reply(tempEmbed);
 
                 } else {
 
-                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${tempPrice} ♏︎`);
+                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${role.temp.price} ♏︎`);
 
                 }
 
-            } else if (message.member.roles.cache.has(tempRole))
+            } else if (message.member.roles.cache.has(role.temp.role))
             {
 
-                if(message.member.roles.cache.has(internRole)) return message.reply("You already have this role");
+                if(message.member.roles.cache.has(role.intern.role)) return message.reply("You already have this role");
 
-                if(data.money >= internPrice)
+                if(data.money >= role.intern.price)
                 {
 
-                    message.member.roles.add(internRole).catch(console.error);
-                    message.member.roles.remove(tempRole).catch(console.error);
-                    data.money -= internPrice;
+                    message.member.roles.add(role.intern.role).catch(console.error);
+                    message.member.roles.remove(role.temp.role).catch(console.error);
+                    data.money -= role.intern.price;
                     data.save().catch(err => console.log(err));
 
                     let internEmbed = new Discord.MessageEmbed();
@@ -136,22 +122,22 @@ module.exports.run = async (bot, message, args) => {
                 } else {
 
                     //user doesn't have enough to purchase this role
-                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${internPrice} ♏︎`);
+                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${role.intern.price} ♏︎`);
 
                 }
 
 
-            } else if(message.member.roles.cache.has(internRole))
+            } else if(message.member.roles.cache.has(role.intern.role))
             {
 
-                if(message.member.roles.cache.has(deskAgentRole)) return message.reply("You already have this role");
+                if(message.member.roles.cache.has(role.deskAgent.role)) return message.reply("You already have this role");
 
-                if(data.money >= deskAgentPrice)
+                if(data.money >= role.deskAgent.price)
                 {
 
-                    message.member.roles.add(deskAgentRole).catch(console.error);
-                    message.member.roles.remove(internRole).catch(console.error);
-                    data.money -= deskAgentPrice;
+                    message.member.roles.add(role.deskAgent.role).catch(console.error);
+                    message.member.roles.remove(role.intern.role).catch(console.error);
+                    data.money -= role.deskAgent.price;
                     data.save().catch(err => console.log(err));
 
                     let deskAgentEmbed = new Discord.MessageEmbed();
@@ -164,63 +150,63 @@ module.exports.run = async (bot, message, args) => {
 
                 } else {
 
-                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${deskAgentPrice} ♏︎`);
+                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${role.deskAgent.price} ♏︎`);
 
                 }
 
-            } else if (message.member.roles.cache.has(deskAgentRole))
+            } else if (message.member.roles.cache.has(role.deskAgent.role))
             {
 
-                if(message.member.roles.cache.has(juniorRole)) return message.reply("You already have this role");
+                if(message.member.roles.cache.has(role.juniorDev.role)) return message.reply("You already have this role");
 
-                if(data.money >= juniorPrice)
+                if(data.money >= role.juniorDev.price)
                 {
 
-                    message.member.roles.add(juniorRole).catch(console.error);
-                    message.member.roles.remove(deskAgentRole).catch(console.error);
-                    data.money -= juniorPrice;
+                    message.member.roles.add(role.juniorDev.role).catch(console.error);
+                    message.member.roles.remove(role.deskAgent.role).catch(console.error);
+                    data.money -= role.juniorDev.price;
                     data.save().catch(err => console.log(err));
 
-                    let juniorRoleEmbed = new Discord.MessageEmbed();
-                    juniorRoleEmbed.setDescription("We can't believe it has been this long and you've stuck with us.\n\nI congratulate you on all of your success!\n Thank you!! Your new paycheck is 2000 ♏︎");
+                    let role.juniorDev.roleEmbed = new Discord.MessageEmbed();
+                    role.juniorDev.roleEmbed.setDescription("We can't believe it has been this long and you've stuck with us.\n\nI congratulate you on all of your success!\n Thank you!! Your new paycheck is 2000 ♏︎");
 
                     
-                    juniorRoleEmbed.setColor(color.purple);
+                    role.juniorDev.roleEmbed.setColor(color.purple);
 
-                    return message.reply(juniorRoleEmbed);
+                    return message.reply(role.juniorDev.roleEmbed);
 
 
                 } else {
 
-                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${juniorPrice} ♏︎`);
+                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${role.juniorDev.price} ♏︎`);
 
                 }
 
-            } else if(message.member.roles.cache.has(seniorRole))
+            } else if(message.member.roles.cache.has(role.seniorDev.role))
             {
 
-                if(message.member.roles.cache.has(seniorRole)) return message.reply("You already have this role");
+                if(message.member.roles.cache.has(role.seniorDev.role)) return message.reply("You already have this role");
 
-                if(data.money >= seniorPrice)
+                if(data.money >= role.seniorDev.price)
                 {
 
-                    message.member.roles.add(seniorRole).catch(console.error);
-                    message.member.roles.remove(juniorRole).catch(console.error);
-                    data.money -= seniorPrice;
+                    message.member.roles.add(role.seniorDev.role).catch(console.error);
+                    message.member.roles.remove(role.juniorDev.role).catch(console.error);
+                    data.money -= role.seniorDev.price;
                     data.save().catch(err => console.log(err));
 
-                    let seniorRoleEmbed = new Discord.MessageEmbed();
-                    seniorRoleEmbed.setDescription("Good job. You've made it. Now instead of learning how to code and being a programmer you're solving everyone's issues\nYou are becoming a great worker and have definitely graduated from StackOverflow\n\nNew paycheck = 3000 ♏︎");
+                    let role.seniorDev.roleEmbed = new Discord.MessageEmbed();
+                    role.seniorDev.roleEmbed.setDescription("Good job. You've made it. Now instead of learning how to code and being a programmer you're solving everyone's issues\nYou are becoming a great worker and have definitely graduated from StackOverflow\n\nNew paycheck = 3000 ♏︎");
 
                     
-                    seniorRoleEmbed.setColor(color.red);
+                    role.seniorDev.roleEmbed.setColor(color.red);
 
-                    return message.reply(seniorRoleEmbed);
+                    return message.reply(role.seniorDev.roleEmbed);
 
 
                 } else {
 
-                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${seniorPrice} ♏︎`);
+                    return message.reply(`Sorry, you do not have enough to purchase this role... This role costs ${role.seniorDev.price} ♏︎`);
 
                 }
 
