522678f1cca7afa7f40fe9b4663abdb9495fa43c
Markay12 <ashinhust.brass@gmail.com>
2020-11-23 13:38:53 -0700

    new update message

--- commands/update.js
+++ commands/update.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
     let embed = new Discord.MessageEmbed();
 
-    embed.setDescription("New Bot Updates as of 12 November 2020\n\n**Added New Role Information!**\n.json file includes all role information. New role available for purchase Senior Developer which costs 20,000.\n Modules and .ranks has been updated as well\n\nContact Markay#5500 for any more information");
+    embed.setDescription("New Bot Updates as of 12 November 2020\n\n*Minor Change*\nFixed issue with donation to user. Now fixed between 0-10000\n\nContact Markay#5500 for any more information");
 
     embed.setColor(color.red);
     return message.channel.send(embed);
